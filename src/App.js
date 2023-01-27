82f68320bb5706e97d534f10ec22d3659b1d4195
stefan.johnbosco <stefan.johnbosco@cgi.com>
2023-01-27 10:06:56 -0500

    Protect Create Route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "./style.scss";
 import "./media-query.css";
 import Home from "./pages/Home";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import Detail from "./pages/Detail";
 import AddEditBlog from "./pages/AddEditBlog";
@@ -51,8 +51,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/create" element={<AddEditBlog user={user} />} />
-        <Route path="/update/:id" element={<AddEditBlog />} />
+        <Route
+          path="/create"
+          element={user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />}
+        />
+        <Route path="/update/:id" element={user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />} />
         <Route path="/about" element={<About />} />
         <Route path="/auth" element={<Auth setActive={setActive} />} />
         <Route path="*" element={<NotFound />} />
