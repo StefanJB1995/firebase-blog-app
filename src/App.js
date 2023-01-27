6c0c36ab5b336eae433b584a125f29f8e9c576cc
stefan.johnbosco <stefan.johnbosco@cgi.com>
2023-01-27 09:42:47 -0500

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./style.scss";
+import "./media-query.css";
+import Home from "./pages/Home";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import Detail from "./pages/Detail";
+import AddEditBlog from "./pages/AddEditBlog";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import { ToastContainer } from "react-toastify";
+import Header from "./components/Header";
+import Auth from "./pages/Auth";
+import { auth } from "./firebase";
+import { signOut } from "firebase/auth";
 
 function App() {
+  const [active, setActive] = useState("home");
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      setUser(null);
+      setActive("login");
+      navigate("/auth");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        setActive={setActive}
+        active={active}
+        user={user}
+        handleLogout={handleLogout}
+      />
+      <ToastContainer position="top-center" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/create" element={<AddEditBlog user={user} />} />
+        <Route path="/update/:id" element={<AddEditBlog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/auth" element={<Auth setActive={setActive} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
