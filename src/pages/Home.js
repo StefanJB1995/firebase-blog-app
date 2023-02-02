901c87a0bad1fd4c11caf06d781c0b6ca9adb4cd
stefan.johnbosco <55599785+StefanJB1995@users.noreply.github.com>
2023-02-02 15:56:37 -0500

    Search Query

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ import {
   getDocs,
   orderBy,
 } from "firebase/firestore";
-import { isEmpty } from "lodash";
+import { isEmpty, isNull } from "lodash";
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import BlogSection from "../components/BlogSection";
@@ -18,6 +18,12 @@ import Spinner from "../components/Spinner";
 import Tags from "../components/Tags";
 import Trending from "../components/Trending";
 import { db } from "../firebase";
+import { useLocation } from "react-router-dom";
+import { async } from "@firebase/util";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
 
 const Home = ({ setActive, user }) => {
   const [loading, setLoading] = useState(true);
@@ -25,6 +31,8 @@ const Home = ({ setActive, user }) => {
   const [tags, setTags] = useState([]);
   const [search, setSearch] = useState("");
   const [trendings, setTrendings] = useState([]);
+  const queryString = useQuery();
+  const searchQuery = queryString.get("searchQuery");
 
   const getTrendingBlogs = async () => {
     const blogRef = collection(db, "blogs");
@@ -66,9 +74,27 @@ const Home = ({ setActive, user }) => {
     };
   }, [setActive]);
 
+  useEffect(() => {
+    if (!isNull(searchQuery)) {
+      //isNull is from package lodash
+      searchBlogs();
+    }
+  }, [searchQuery]);
+
   if (loading) {
     return <Spinner />;
   }
+
+  const searchBlogs = async () => {
+    const blogRef = collection(db, "blogs");
+    const searchTitleQuery = query(blogRef, where("title", "==", searchQuery));
+    const titleSnapshot = await getDocs(searchTitleQuery);
+    let searchTitleBlogs = [];
+    titleSnapshot.forEach((doc) => {
+      searchTitleBlogs.push({ id: doc.id, ...doc.data() });
+    });
+    setBlogs(searchTitleBlogs);
+  };
 
   const handleDelete = async (id) => {
     if (window.confirm("Are you sure you want to delete this blog?")) {
@@ -85,8 +111,8 @@ const Home = ({ setActive, user }) => {
 
   const getBlogs = async () => {
     const blogRef = collection(db, "blogs");
-    const blogsQuery = query(blogRef, orderBy("title"));
-    const docSnapshot = await getDocs(blogsQuery);
+    // const blogsQuery = query(blogRef, orderBy("title"));
+    const docSnapshot = await getDocs(blogRef);
     setBlogs(docSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
   };
 
